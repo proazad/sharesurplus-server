bd817cf67195910769f76867a7d647894d3367d0
proazad <proazadhossain7595@gmail.com>
2023-11-06 11:57:23 +0300

    Get All Foods Complete

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const cookieparser = require("cookie-parser");
+const cookieParser = require("cookie-parser")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cookieparser());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7gpv70.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,11 +23,43 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+        const foodCollection = client.db("shareSurplus").collection("foodsCollection");
+        const foodRequestCollection = client.db("shareSurplus").collection("foodRequestCollection");
+
+
+        //  * Services Related API 
+        //  * add Foods
+
+        app.post("/foods", async (req, res) => {
+            const food = req.body;
+            const result = await foodCollection.insertOne(food);
+            res.send(result);
+        });
+
+        // get all Foods 
+        app.get("/foods", async (req, res) => {
+            const cursot = foodCollection.find();
+            const result = await cursot.toArray();
+            res.send(result);
+        });
+
+        // get personal Foods 
+        app.get("/foods", verifyToken, async (req, res) => {
+            let query = {};
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send("Access Forbbiden");
+            }
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -38,10 +70,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.use("/", async (req, res) => {
-    console.log("Share Surplus Server is Running!");
-})
+app.get("/", (req, res) => {
+    res.send("shareSurplus Server is Running");
+});
 
 app.listen(port, () => {
-    console.log(`share surplus server is running on ${port}`);
-})
+    console.log(`shareSurplus Server is Running on Port: ${port}`);
+});
